7e8369c1276dfb35202eb8a67e02397ce3bef1b5
d3xsky <dmisic@gmail.com>
2018-12-12 02:19:53 +0100

    Second Task

--- main.cpp
+++ main.cpp
@@ -16,7 +16,45 @@ public:
     string gender;
     int yearOfBirth;
 };
+struct node{
+    double data;
+    node *link;
+};
 
+node* populateList(){
+node *first, *newNode, *last;
+double num;
+cout << "Enter a list of doubles ending with 0."
+<< endl;
+cin >> num;
+first = NULL;
+while (num != 0.0)
+{
+    newNode = new node;
+    newNode->data = num;
+    newNode->link = NULL;
+    if (first == NULL)
+    {
+        first = newNode;
+        last = newNode;
+    }
+    else
+    {
+        last->link = newNode;
+        last = newNode;
+    }
+    cin >> num;
+    //end while
+}
+    return first;
+}
+void PrintList(struct node* list){
+    
+    if (list == NULL) return;
+    cout << list->data << "\n";
+    PrintList(list->link);
+    
+}
 //Function for checking for an existing id
 bool checkForId(int id, Student *studentArray, int studentNumber){
     int sizeOfArray = sizeof(studentArray);
@@ -30,37 +68,49 @@ bool checkForId(int id, Student *studentArray, int studentNumber){
 
 int main()
 {
-    //Declaration of students array by prompting user to enter number of students
-    int numOfSudents;
-    Student *studentArray = 0;
-    
-    cout << "Enter the number of students: ";
-    cin >> numOfSudents;
-    
-    studentArray = new Student[numOfSudents];
-    
-    //Entering information for each student
-    for(int i=0; i<numOfSudents; i++){
-        // Prompt user to enter id until it is unique
-        do{
-            cout << "Enter the id for student number " << i+1 << " ";
-            cin >> (studentArray+i)->id;
-        }while(checkForId((studentArray+i)->id, studentArray, i));
-        cout << "Enter the gender for student number " << i+1 << " ";
-        cin >> (studentArray+i)->gender;
-        cout << "Enter the year of birth for student number " << i+1 << " ";
-        cin >> (studentArray+i)->yearOfBirth;
-    }
-    
-    //Outputing Students information
-    for(int i=0; i<numOfSudents; i++){
-        cout << "Studnet number " << i+1 << " has id: " << (studentArray+i)->id << endl;
-        cout << "Studnet number " << i+1 << " has gender: " << (studentArray+i)->gender << endl;
-        cout << "Studnet number " << i+1 << " has year of birth: " << (studentArray+i)->yearOfBirth << endl;
-    }
-    
-    //Delete pointer to an array its a good practice
-    delete []studentArray;
-    
+    /*
+      --> UNCOMMENT BELOW FOR FIRST TASK TO EXECUTE <--
+     */
+    ////Declaration of students array by prompting user to enter number of students
+    //int numOfSudents;
+    //Student *studentArray = 0;
+    //
+    //cout << "Enter the number of students: ";
+    //cin >> numOfSudents;
+    //
+    //studentArray = new Student[numOfSudents];
+    //
+    ////Entering information for each student
+    //for(int i=0; i<numOfSudents; i++){
+    //    // Prompt user to enter id until it is unique
+    //    do{
+    //        cout << "Enter the id for student number " << i+1 << " ";
+    //        cin >> (studentArray+i)->id;
+    //    }while(checkForId((studentArray+i)->id, studentArray, i));
+    //    cout << "Enter the gender for student number " << i+1 << " ";
+    //    cin >> (studentArray+i)->gender;
+    //    cout << "Enter the year of birth for student number " << i+1 << " ";
+    //    cin >> (studentArray+i)->yearOfBirth;
+    //}
+    //
+    ////Outputing Students information
+    //for(int i=0; i<numOfSudents; i++){
+    //    cout << "Studnet number " << i+1 << " has id: " << (studentArray+i)->id << endl;
+    //    cout << "Studnet number " << i+1 << " has gender: " << (studentArray+i)->gender << endl;
+    //    cout << "Studnet number " << i+1 << " has year of birth: " << (studentArray+i)->yearOfBirth << endl;
+    //}
+    //
+    ////Delete pointer to an array its a good practice
+    //delete []studentArray;
+    /*
+     --> UNCOMMENT BELOW FOR SECOND TASK TO EXECUTE <--
+     */
+    //node* nudla;
+    //nudla = populateList(); // view function above which populate the list by building it forward
+    //PrintList(nudla); // simple recursive function to print each node
+    /*
+     --> UNCOMMENT BELOW FOR third TASK TO EXECUTE <--
+     */
     return 0;
 }
+
