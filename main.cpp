9d55a1023c0dbcc2ac8c6ea9677773b5ddf8169c
d3xsky <dmisic@gmail.com>
2018-12-12 01:57:39 +0100

    First Task

--- main.cpp
+++ main.cpp
@@ -7,9 +7,60 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//Class of student
+class Student{
+public:
+    int id;
+    string gender;
+    int yearOfBirth;
+};
+
+//Function for checking for an existing id
+bool checkForId(int id, Student *studentArray, int studentNumber){
+    int sizeOfArray = sizeof(studentArray);
+    for(int i=0;i<sizeOfArray;i++){
+        if(studentNumber != i && id == (studentArray+i)->id){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    //Declaration of students array by prompting user to enter number of students
+    int numOfSudents;
+    Student *studentArray = 0;
+    
+    cout << "Enter the number of students: ";
+    cin >> numOfSudents;
+    
+    studentArray = new Student[numOfSudents];
+    
+    //Entering information for each student
+    for(int i=0; i<numOfSudents; i++){
+        // Prompt user to enter id until it is unique
+        do{
+            cout << "Enter the id for student number " << i+1 << " ";
+            cin >> (studentArray+i)->id;
+        }while(checkForId((studentArray+i)->id, studentArray, i));
+        cout << "Enter the gender for student number " << i+1 << " ";
+        cin >> (studentArray+i)->gender;
+        cout << "Enter the year of birth for student number " << i+1 << " ";
+        cin >> (studentArray+i)->yearOfBirth;
+    }
+    
+    //Outputing Students information
+    for(int i=0; i<numOfSudents; i++){
+        cout << "Studnet number " << i+1 << " has id: " << (studentArray+i)->id << endl;
+        cout << "Studnet number " << i+1 << " has gender: " << (studentArray+i)->gender << endl;
+        cout << "Studnet number " << i+1 << " has year of birth: " << (studentArray+i)->yearOfBirth << endl;
+    }
+    
+    //Delete pointer to an array its a good practice
+    delete []studentArray;
+    
     return 0;
 }
